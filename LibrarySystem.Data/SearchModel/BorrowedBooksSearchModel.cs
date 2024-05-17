25d064d7a8df3fec60d1e02905362b02c1da1e54
Jomiel <enriquez.jliquigan@gmail.com>
2024-05-17 13:05:40 +0800

    Code Update

--- LibrarySystem.Data/SearchModel/BorrowedBooksSearchModel.cs
+++ LibrarySystem.Data/SearchModel/BorrowedBooksSearchModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibrarySystem.Data.Entities
+namespace LibrarySystem.Data.SearchModel
 {
     public class BorrowedBooksSearchModel
     {
